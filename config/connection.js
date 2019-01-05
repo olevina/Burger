6c751fa07c089ed56c4b207e31741ad038005483
Olevina <olevina@ymail.com>
2019-01-04 23:34:52 -0700

    ong:

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host:"localhost",
-    port:'3306',
-    user:"root",
-    password: "229535Jkz",
-    database: "burgers_db"
-})
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+   }
+   else{
+    var connection = mysql.createConnection({
+        host:"localhost",
+        port:'3306',
+        user:"root",
+        password: "229535Jkz",
+        database: "burgers_db"
+    })
+   }
+
 
 
 connection.connect(function(err){
